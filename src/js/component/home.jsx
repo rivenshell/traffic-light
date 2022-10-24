e3f7ed5d47d6d4625164211edb41a5a5c37fdca9
Riv <rivenshell111@gmail.com>
2022-10-24 23:44:11 +0000

    Initial Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,12 +14,18 @@ const Home = () => {
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
+
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			
+			<div className="traffic-ight">
+			
+			</div>
 		</div>
+	
 	);
 };
 
